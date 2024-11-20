86829714242512504fcf1dab0f202f5f9724b2d2
floalzaix <floalzfencing@gmail.com>
2024-11-20 10:17:17 +0100

    Début tp2

---
+++ bdd/query.sql
@@ -0,0 +1,10 @@
+USE WEB;
+
+CREATE TABLE unit (
+    id VARCHAR(50),
+    name VARCHAR(50) NOT NULL,
+    cost INT NOT NULL,
+    origin VARCHAR(100) NOT NULL,
+    url_img VARCHAR(2090) NOT NULL
+);
+

aca846e3ccab8ceba45ca2cda01dd2919d0d278b
floalzaix <floalzfencing@gmail.com>
2024-12-22 18:21:27 +0100

    Version server finalisé avec les requêtes fonctionnelles

--- bdd/create_tables.sql
+++ bdd/create_tables.sql
@@ -12,20 +12,20 @@ CREATE TABLE units (
     cost INT NOT NULL,
     url_img TEXT NOT NULL,
     PRIMARY KEY (id)
-) ENGINE="InnoDB" CHARSET utf8mb4;
+);
 
 CREATE TABLE origins (
     id VARCHAR(50),
     name VARCHAR(50) NOT NULL,
     url_img TEXT NOT NULL,
     PRIMARY KEY (id)
-) ENGINE="InnoDB" CHARSET utf8mb4;
+);
 
 CREATE TABLE units_origins (
     id_unit VARCHAR(50),
     id_origin VARCHAR(50),
     FOREIGN KEY (id_unit) REFERENCES units(id) ON DELETE CASCADE,
     FOREIGN KEY (id_origin) REFERENCES origins(id) ON DELETE CASCADE
-) ENGINE="InnoDB" CHARSET utf8mb4;
+);
 
 SELECT * FROM origins;
